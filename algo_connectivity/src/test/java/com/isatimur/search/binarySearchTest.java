ce869888360f40c97ccc19aa1f4b79e6c7bd9a01
Якименко Артём Борисович <tilorn1@mail.ru>
2016-11-13 23:52:19 +0300

    binarySearch.java Ctrl+Alt+L -refactoring a little

--- algo_connectivity/src/test/java/com/isatimur/search/binarySearchTest.java
+++ algo_connectivity/src/test/java/com/isatimur/search/binarySearchTest.java
@@ -4,10 +4,12 @@ import junit.framework.TestCase;
 
 /**
  * Created by abyakimenko on 10.11.2016.
+ * need a sorted array
+ * algorithm complexity is (1 + lgN)
  */
 public class binarySearchTest extends TestCase {
 
-    private final int[] arrayOfInt = new int[]{1, 2, 3, 4, 8, 11, 14, 18, 20, 22, 26, 29, 35, 43, 48, 57, 65};
+    private final int[] arrayOfInt = new int[]{1, 2, 3, 4, 8, 11, 14, 14, 18, 20, 22, 26, 29, 35, 43, 48, 57, 65};
 
     public void setUp() throws Exception {
         super.setUp();
@@ -15,8 +17,9 @@ public class binarySearchTest extends TestCase {
 
     public void testExecuteSearch() throws Exception {
 
-        assertEquals(15, binarySearch.executeSearch(arrayOfInt, 57));
-        assertEquals(16, binarySearch.executeSearch(arrayOfInt, 65));
+        assertEquals(16, binarySearch.executeSearch(arrayOfInt, 57));
+        assertEquals(17, binarySearch.executeSearch(arrayOfInt, 65));
+        assertEquals(6, binarySearch.executeSearch(arrayOfInt, 14));
         assertEquals(-1, binarySearch.executeSearch(arrayOfInt, 23));
         assertEquals(-1, binarySearch.executeSearch(arrayOfInt, 101));
     }
