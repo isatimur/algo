79af4973b72069588024b4278378754214029aa4
Якименко Артём Борисович <tilorn1@mail.ru>
2016-11-14 09:09:13 +0300

    implement shell sorting

--- stack_queues/src/test/java/elementary/sort/insertionSortTest.java
+++ stack_queues/src/test/java/elementary/sort/insertionSortTest.java
@@ -7,8 +7,14 @@ import junit.framework.TestCase;
  */
 public class insertionSortTest extends TestCase {
 
-        private final int[] arrayOfInts = new int[]{11, 21, 3, 41, 8, 11, 14, 14, 18, 20, 22, 26, 29, 35, 43, 48, 57, 65, 1, 2};
+    private int[] arrayOfInts;
 //    private final int[] arrayOfInts = new int[]{11, 21, 3, 41, 8, 2};
+
+
+    @Override
+    protected void setUp() throws Exception {
+        arrayOfInts = new int[]{11, 21, 3, 41, 8, 11, 14, 14, 18, 20, 22, 26, 29, 35, 43, 48, 57, 65, 1, 2};
+    }
 
     public void testExecuteSorting() throws Exception {
 
@@ -17,6 +23,16 @@ public class insertionSortTest extends TestCase {
         assertEquals(arrayOfInts[0], 1);
         assertEquals(arrayOfInts[1], 2);
         assertEquals(arrayOfInts[3], 8);
+    }
+
+    public void testShellExecuteSorting() throws Exception {
+
+        Comparable[] array = new Comparable[]{22, 11, 44, 57, 54, 57, 62, 42, 21, 11};
+        shellSort.executeSorting(array);
+
+        assertEquals(array[0], 11);
+        assertEquals(array[1], 11);
+        assertEquals(array[3], 22);
     }
 
 }
