296dd3881180bc32bc92b499fbc9dacd040a9f9b
Якименко Артём Борисович <tilorn1@mail.ru>
2016-11-17 01:34:28 +0300

    changes in my implementation of queues

--- week_tasks/src/main/java/com/learning/princeton/algo/tasks/week_2/abyakimenko/entities/SubsetList.java
+++ week_tasks/src/main/java/com/learning/princeton/algo/tasks/week_2/abyakimenko/entities/SubsetList.java
@@ -8,11 +8,19 @@ import java.util.Collections;
 import java.util.List;
 
 public class SubsetList {
-	 public static void main(String[] args) {
-		 int k = Integer.parseInt(args[0]);
-		 List<String> queue = new ArrayList<String>();
-		 while(!StdIn.isEmpty()) queue.add(StdIn.readString());
-		 Collections.shuffle(queue);
-		 for(int i=0;i<k;i++) StdOut.println(queue.get(i));
-	 }
+
+    private SubsetList() {
+    }
+
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        List<String> queue = new ArrayList<>();
+        while (!StdIn.isEmpty()) {
+            queue.add(StdIn.readString());
+        }
+        Collections.shuffle(queue);
+        for (int i = 0; i < k; i++) {
+            StdOut.println(queue.get(i));
+        }
+    }
 }
