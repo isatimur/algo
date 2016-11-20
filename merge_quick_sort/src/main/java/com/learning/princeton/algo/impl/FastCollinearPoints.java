fad5d44aa4aebbbfc77bdea1d32d0f353c592d0f
Якименко Артём Борисович <tilorn1@mail.ru>
2016-11-21 00:18:54 +0300

    FasterCollinearPoints

--- merge_quick_sort/src/main/java/com/learning/princeton/algo/impl/FastCollinearPoints.java
+++ merge_quick_sort/src/main/java/com/learning/princeton/algo/impl/FastCollinearPoints.java
@@ -3,36 +3,47 @@ package com.learning.princeton.algo.impl;
 import com.learning.princeton.algo.search.LineSegment;
 import com.learning.princeton.algo.types.Point;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A faster, sorting-based solution. Remarkably, it is possible to solve the problem much faster than the brute-force
  * solution described above. Given a point p, the following method determines whether p participates in a set of 4
  * or more collinear points.
 
  Think of p as the origin.
- For each other point q, determine the slope it makes with p.
- Sort the points according to the slopes they makes with p.
- Check if any 3 (or more) adjacent points in the sorted order have equal slopes with respect to p. If so, these points,
+ - For each other point q, determine the slope it makes with p.
+ - Sort the points according to the slopes they makes with p.
+ - Check if any 3 (or more) adjacent points in the sorted order have equal slopes with respect to p. If so, these points,
  together with p, are collinear.
- Applying this method for each of the n points in turn yields an efficient algorithm to the problem. The algorithm
+
+ !!!
+ Applying this method for each of the n points in turn yields an efficient algorithm to the problem.
+ !!!
+
+ The algorithm
  solves the problem because points that have equal slopes with respect to p are collinear, and sorting brings such
  points together. The algorithm is fast because the bottleneck operation is sorting.
  */
 public class FastCollinearPoints {
 
+    private List<LineSegment> lineSegments = new ArrayList<>();
+
     // finds all line segments containing 4 or more points
     public FastCollinearPoints(Point[] points) {
+
+        Point[] pointsCopy = Arrays.copyOf(points, points.length);
 
     }
 
     // the number of line segments
     public int numberOfSegments() {
-
-        return 0;
+        return lineSegments.size();
     }
 
     // the line segments
     public LineSegment[] segments(){
-
-        return new LineSegment[0];
+        return lineSegments.toArray(new LineSegment[numberOfSegments()]);
     }
 }
