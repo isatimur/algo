3eefdc5a4aca7ee17dd59628041cf8b9e6c5c5ff
Якименко Артём Борисович <tilorn1@mail.ru>
2016-12-01 00:40:07 +0300

    week 4 ends!

---
+++ heaps/src/main/java/com/learning/princeton/algo/Board.java
@@ -0,0 +1,58 @@
+package com.learning.princeton.algo;
+
+public class Board {
+
+    public Board(int[][] blocks){}           // construct a board from an n-by-n array of blocks
+
+                                           // (where blocks[i][j] = block in row i, column j)
+
+    // board dimension n
+    public int dimension(){
+
+        return 0;
+    }
+
+    // number of blocks out of place
+    public int hamming(){
+
+        return 0;
+    }
+
+    // sum of Manhattan distances between blocks and goal
+    public int manhattan(){
+
+        return 0;
+    }
+
+    // is this board the goal board?
+    public boolean isGoal() {
+
+        return false;
+    }
+
+    // a board that is obtained by exchanging any pair of blocks
+    public Board twin() {
+
+        return null;
+    }
+
+    // does this board equal y?
+    public boolean equals(Object y){
+
+        return false;
+    }
+
+    // all neighboring boards
+    public Iterable<Board> neighbors() {
+
+        return null;
+    }
+
+    // string representation of this board (in the output format specified below)
+    public String toString(){
+
+        return null;
+    }
+
+    public static void main(String[] args){} // unit tests (not graded)
+}
